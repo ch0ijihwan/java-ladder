da5623f2b8fb1151e2df6be9116da5feaf3a28e8
cjh8746 <cjh87467@gmail.com>
2022-05-31 15:31:10 +0900

    refactor : 변수명 변경

--- src/main/java/model/ladder/LineFactory.java
+++ src/main/java/model/ladder/LineFactory.java
@@ -35,10 +35,10 @@ public class LineFactory {
         return middlePoints;
     }
 
-    private static List<Point> combine(final List<Point> points, final List<Point> points2) {
+    private static List<Point> combine(final List<Point> points, final List<Point> pointsToMerged) {
         List<Point> joinedPoints = new ArrayList<>();
         joinedPoints.addAll(points);
-        joinedPoints.addAll(points2);
+        joinedPoints.addAll(pointsToMerged);
         return joinedPoints;
     }
 
