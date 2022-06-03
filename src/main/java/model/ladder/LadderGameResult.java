9ff3e1b2d4f4df0bf0d7d66bec6c55c7d468d50a
cjh8746 <cjh87467@gmail.com>
2022-06-03 11:48:54 +0900

    feat : LadderGameResult 구현

---
+++ src/main/java/model/ladder/LadderGameResult.java
@@ -0,0 +1,28 @@
+package model.ladder;
+
+import java.util.Map;
+import java.util.Set;
+
+public class LadderGameResult {
+
+    private final Map<String, String> result;
+
+    public LadderGameResult(final Map<String, String> result) {
+        this.result = result;
+    }
+
+    public String getResult(final String name) {
+        if (!result.containsKey(name)) {
+            throw new IllegalArgumentException("존재하지 않는 플레이어의 이름을 입력하였습니다.");
+        }
+        return result.get(name);
+    }
+
+    public Set<Map.Entry<String, String>> getAllResult(){
+        return result.entrySet();
+    }
+
+    public int getSize() {
+        return result.size();
+    }
+}
