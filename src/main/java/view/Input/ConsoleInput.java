c58b79a443c351f23a7e4f002b8ffb50aafa50dc
cjh8746 <cjh87467@gmail.com>
2022-06-04 15:47:27 +0900

    refactor : 매직넘버로 분리

--- src/main/java/view/Input/ConsoleInput.java
+++ src/main/java/view/Input/ConsoleInput.java
@@ -8,11 +8,15 @@ import java.util.stream.Collectors;
 public class ConsoleInput implements Input {
 
     private static final String DELIMITER = ",";
+    private static final String PLAYERS_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(%s)로 구문하세요)%n";
+    private static final String REWARDS_INPUT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(%s)로 구문하세요)%n";
+    private static final String LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 인가요?";
+    private static final String RESULT_OF_PLAYER_INPUT_MESSAGE = "결과를 보고 싶은 사람은?";
     private static final Scanner SCANNER = new Scanner(System.in);
 
     @Override
     public List<String> inputNames() {
-        System.out.printf("참여할 사람 이름을 입력하세요. (이름은 쉼표(%s)로 구문하세요)%n", DELIMITER);
+        System.out.printf(PLAYERS_INPUT_MESSAGE, DELIMITER);
         return Arrays.stream(inputSplitNames())
                 .collect(Collectors.toUnmodifiableList());
     }
@@ -24,20 +28,20 @@ public class ConsoleInput implements Input {
 
     @Override
     public List<String> inputRewards() {
-        System.out.printf("실행 결과를 입력하세요. (결과는 쉼표(%s)로 구문하세요)\n", DELIMITER);
+        System.out.printf(REWARDS_INPUT_MESSAGE, DELIMITER);
         return Arrays.stream(inputSplitNames())
                 .collect(Collectors.toUnmodifiableList());
     }
 
     @Override
     public int inputLadderHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(LADDER_HEIGHT_INPUT_MESSAGE);
         return SCANNER.nextInt();
     }
 
     @Override
     public String inputTargetPlayerName() {
-        System.out.println("결과를 보고 싶은 사람은?");
+        System.out.println(RESULT_OF_PLAYER_INPUT_MESSAGE);
         SCANNER.nextLine();
         return SCANNER.nextLine();
     }
