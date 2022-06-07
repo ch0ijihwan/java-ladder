68b0e48db876962bb62ef908e8e1c6a8d9ba7077
cjh8746 <cjh87467@gmail.com>
2022-06-07 16:54:21 +0900

    refactor : RandomLinkableStrategy 싱글톤으로 변경

--- src/main/java/model/ladder/linkablestrategy/RandomLinkableStrategy.java
+++ src/main/java/model/ladder/linkablestrategy/RandomLinkableStrategy.java
@@ -6,6 +6,15 @@ public class RandomLinkableStrategy implements LinkableStrategy {
 
     private static final Random RANDOM = new Random();
 
+    private static final RandomLinkableStrategy randomLinkableStrategy = new RandomLinkableStrategy();
+
+    public static RandomLinkableStrategy getInstance() {
+        return randomLinkableStrategy;
+    }
+
+    private RandomLinkableStrategy() {
+    }
+
     @Override
     public boolean generateLinkable() {
         return RANDOM.nextBoolean();
